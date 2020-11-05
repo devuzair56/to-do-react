8df1fac8a9cf692afaae13314ad76d18f7b24849
Uzair Iftikhar <hiraifti95@yahoo.com>
2020-11-05 16:41:10 +0500

    Updates

---
+++ precache-manifest.5a0b57f66c7e320e04e21cd992f12ab1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d1a83731906ab997edaf1c756a3bef7",
+    "url": "/to-do-react/index.html"
+  },
+  {
+    "revision": "0b595c21a82da012359e",
+    "url": "/to-do-react/static/css/2.cd37d48a.chunk.css"
+  },
+  {
+    "revision": "9f286b735f01d6e0af57",
+    "url": "/to-do-react/static/css/main.88f6cf77.chunk.css"
+  },
+  {
+    "revision": "0b595c21a82da012359e",
+    "url": "/to-do-react/static/js/2.2b185223.chunk.js"
+  },
+  {
+    "revision": "9f286b735f01d6e0af57",
+    "url": "/to-do-react/static/js/main.5a30d0eb.chunk.js"
+  },
+  {
+    "revision": "656b3439b57802ac4816",
+    "url": "/to-do-react/static/js/runtime~main.c894c534.js"
+  },
+  {
+    "revision": "c114616893962e8edab59b9e073f60ee",
+    "url": "/to-do-react/static/media/bubble-invert.c1146168.svg"
+  },
+  {
+    "revision": "670eb41fdb462eacbb876c3dc23f8b09",
+    "url": "/to-do-react/static/media/bubble.670eb41f.svg"
+  },
+  {
+    "revision": "3326c360137c2d6a6909d0c4303e502a",
+    "url": "/to-do-react/static/media/icons-16.3326c360.eot"
+  },
+  {
+    "revision": "895bc7de59413bb8029ac83cac8c9e1b",
+    "url": "/to-do-react/static/media/icons-16.895bc7de.woff"
+  },
+  {
+    "revision": "b6382c6d9ffca4a1c50f58a17fce15a9",
+    "url": "/to-do-react/static/media/icons-16.b6382c6d.ttf"
+  },
+  {
+    "revision": "058e48cbbee6cdbfb2a2e6dab347f245",
+    "url": "/to-do-react/static/media/icons-20.058e48cb.woff"
+  },
+  {
+    "revision": "aa8f3ca044cd237f87b69d0cb364887a",
+    "url": "/to-do-react/static/media/icons-20.aa8f3ca0.ttf"
+  },
+  {
+    "revision": "e666d692a7dc07ea69886dfcec837c3c",
+    "url": "/to-do-react/static/media/icons-20.e666d692.eot"
+  }
+]);
